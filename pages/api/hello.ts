e5d6b0dd4804e5dc3501f15753e1338b0689ae28
rishit <rishitmohan@gmail.com>
2023-01-23 11:44:37 +0530

    working

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration ,OpenAIApi} from 'openai'
 
-type Data = {
-  name: string
-}
 
-export default function handler(
+
+const configuration= new Configuration({
+  apiKey:process.env.OPEN_API_KRY,
+})
+
+const openai=new OpenAIApi(configuration)
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
+
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prompt=req.query.prompt
+  if(!prompt){
+    return res.status(400).json({error:" missing prompt"})
+  }
+if(prompt.length>100){
+  return res.status(400).json({error:"  prompt too long "})
+
 }
+
+const completion=await openai.createCompletion({
+  model:"text-davinci-003",
+  prompt:`create a cool business name  for ${ prompt}. \n
+  Funny Business name is :
+  `
+,
+max_tokens:500,
+temperature:1,
+presence_penalty:0,
+frequency_penalty:0
+
+})
+
+console.log("data",completion.data.choices[0])
+
+ const idea=completion.data.choices[0].text
+console.log("idea",idea)
+  res.status(200).json({ idea })
+
+
+ 
+}
+
